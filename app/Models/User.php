9ae9fbe8ed84f4d6256e6006d923cb2d23882342
Ferdinando Ciotola <ferdinandociotola@gmail.com>
2025-12-18 20:36:56 +0000

    Day 30: Database design + Models whit relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,16 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+
+public function tasks()
+{
+    return $this->hasMany(Task::class);
+}
+
+public function categories()
+{
+    return $this->hasMany(Category::class);
+}
+
 }
